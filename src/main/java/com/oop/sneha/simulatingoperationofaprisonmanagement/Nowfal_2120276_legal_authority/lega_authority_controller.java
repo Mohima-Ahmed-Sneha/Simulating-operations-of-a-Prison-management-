480406b9ffd6b8c871d798d5b5edfc4d04ed2f62
Md Nowfal Ali Rahib <2120276@iub.edu.bd>
2026-04-04 21:54:14 +0600

    update02

--- src/main/java/com/oop/sneha/simulatingoperationofaprisonmanagement/Nowfal_2120276_legal_authority/lega_authority_controller.java
+++ src/main/java/com/oop/sneha/simulatingoperationofaprisonmanagement/Nowfal_2120276_legal_authority/lega_authority_controller.java
@@ -1,7 +1,42 @@
 package com.oop.sneha.simulatingoperationofaprisonmanagement.Nowfal_2120276_legal_authority;
 
+import javafx.event.ActionEvent;
+
 public class lega_authority_controller
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void IncidentButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void IssueWarrantButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void PayroleButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ConsultationButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void FacilityAuditButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void CropusPetitionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ReleaseButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void InterFacilityTransferButton(ActionEvent actionEvent) {
+    }
+}
