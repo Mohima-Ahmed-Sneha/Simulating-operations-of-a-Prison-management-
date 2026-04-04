7b9b11f7ebb817fc66c0ef241719846b30ff9917
Mohima_Ahmed_Sneha <mohimaahmedsneha@gmail.com>
2026-04-04 17:34:22 +0600

    init commit4

---
+++ src/main/java/com/oop/sneha/simulatingoperationofaprisonmanagement/mohima_2110887_rehabilitation_officer/enroll_inmateController.java
@@ -0,0 +1,34 @@
+package com.oop.sneha.simulatingoperationofaprisonmanagement.mohima_2110887_rehabilitation_officer;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class enroll_inmateController
+{
+    @javafx.fxml.FXML
+    private Label inmateIDTF;
+    @javafx.fxml.FXML
+    private Label programmeIDTF;
+    @javafx.fxml.FXML
+    private Label statusL;
+    @javafx.fxml.FXML
+    private TableView enrollmentTV;
+    @javafx.fxml.FXML
+    private TableColumn programmeTC;
+    @javafx.fxml.FXML
+    private TableColumn dateTC;
+    @javafx.fxml.FXML
+    private Label enrolllmentL;
+    @javafx.fxml.FXML
+    private TableColumn inmateTC;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void enrollButton(ActionEvent actionEvent) {
+    }
+}
