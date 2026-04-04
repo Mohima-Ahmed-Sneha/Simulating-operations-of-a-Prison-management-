9f947a87c601f546a46af016b4ce615a45c3a576
Mohima_Ahmed_Sneha <mohimaahmedsneha@gmail.com>
2026-04-04 18:43:56 +0600

    init commit6

---
+++ src/main/java/com/oop/sneha/simulatingoperationofaprisonmanagement/mohima_2110887_medical_officer/inventoryController.java
@@ -0,0 +1,7 @@
+package com.oop.sneha.simulatingoperationofaprisonmanagement.mohima_2110887_medical_officer;
+
+public class inventoryController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
