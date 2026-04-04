b7332704ec6277c0a3dc2f8b8257a647531112dd
Mohima_Ahmed_Sneha <mohimaahmedsneha@gmail.com>
2026-04-04 16:48:37 +0600

    init commit2

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,5 +4,8 @@ module com.oop.sneha.simulatingoperationofaprisonmanagement {
 
 
     opens com.oop.sneha.simulatingoperationofaprisonmanagement to javafx.fxml;
+    opens com.oop.sneha.simulatingoperationofaprisonmanagement.mohima_2110887_rehabilitation_officer to javafx.fxml, java.base;
+    opens com.oop.sneha.simulatingoperationofaprisonmanagement.mohima_2110887_medical_officer to javafx.fxml, java.base;
+
     exports com.oop.sneha.simulatingoperationofaprisonmanagement;
 }
