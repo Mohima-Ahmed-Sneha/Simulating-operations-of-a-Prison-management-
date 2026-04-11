4d5228923908590e3bc76f5b02b09d788cc18678
Md Nowfal Ali Rahib <2120276@iub.edu.bd>
2026-04-11 14:59:37 +0600

    update 05

--- src/main/java/com/oop/sneha/simulatingoperationofaprisonmanagement/Nowfal_2120276_accountant_officer/facilityvadalism_controller.java
+++ src/main/java/com/oop/sneha/simulatingoperationofaprisonmanagement/Nowfal_2120276_accountant_officer/facilityvadalism_controller.java
@@ -1,7 +1,27 @@
 package com.oop.sneha.simulatingoperationofaprisonmanagement.Nowfal_2120276_accountant_officer;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class facilityvadalism_controller
 {
     @javafx.fxml.FXML
+    private Label displayLabel;
+    @javafx.fxml.FXML
+    private ComboBox<String> inventoryCombo;
+    @javafx.fxml.FXML
+    private TextField inmateIdText;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+        inventoryCombo.getItems().addAll("Bed","chair","table");
+    }
+
+    @javafx.fxml.FXML
+    public void calculatebutton(ActionEvent actionEvent) {
+
+
+    }
+}
