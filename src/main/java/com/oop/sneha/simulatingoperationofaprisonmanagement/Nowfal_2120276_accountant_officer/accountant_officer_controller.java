21e69f3adc591c7cd5f184ec46af594feafebe90
Md Nowfal Ali Rahib <2120276@iub.edu.bd>
2026-04-11 19:16:38 +0600

    update 06 vandalism

--- src/main/java/com/oop/sneha/simulatingoperationofaprisonmanagement/Nowfal_2120276_accountant_officer/accountant_officer_controller.java
+++ src/main/java/com/oop/sneha/simulatingoperationofaprisonmanagement/Nowfal_2120276_accountant_officer/accountant_officer_controller.java
@@ -1,6 +1,14 @@
 package com.oop.sneha.simulatingoperationofaprisonmanagement.Nowfal_2120276_accountant_officer;
 
+import com.oop.sneha.simulatingoperationofaprisonmanagement.HelloApplication;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class accountant_officer_controller
 {
@@ -10,6 +18,22 @@ public class accountant_officer_controller
 
     @javafx.fxml.FXML
     public void vandalismButton(ActionEvent actionEvent) {
+
+        Parent root = null;
+
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource( "/com/oop/sneha/simulatingoperationofaprisonmanagement/Nowfal_2120276_accountant_officer/facilityvandalism-view.fxml"));
+            root = fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Scene scene = new Scene(root);
+        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("login");
+        stage.show();
+
     }
 
     @javafx.fxml.FXML
@@ -38,5 +62,24 @@ public class accountant_officer_controller
 
     @javafx.fxml.FXML
     public void grantfundingButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void signoutButton(ActionEvent actionEvent) {
+
+        Parent root = null;
+
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource( "/com/oop/sneha/simulatingoperationofaprisonmanagement/login.fxml"));
+            root = fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Scene scene = new Scene(root);
+        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("login");
+        stage.show();
     }
 }
--- src/main/java/com/oop/sneha/simulatingoperationofaprisonmanagement/Nowfal_2120276_accountant_officer/facilityvadalism_controller.java
+++ src/main/java/com/oop/sneha/simulatingoperationofaprisonmanagement/Nowfal_2120276_accountant_officer/facilityvadalism_controller.java
@@ -1,27 +1,67 @@
 package com.oop.sneha.simulatingoperationofaprisonmanagement.Nowfal_2120276_accountant_officer;
 
+import com.oop.sneha.simulatingoperationofaprisonmanagement.HelloApplication;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-public class facilityvadalism_controller
-{
+import java.io.IOException;
+
+
+public class facilityvadalism_controller {
     @javafx.fxml.FXML
     private Label displayLabel;
     @javafx.fxml.FXML
     private ComboBox<String> inventoryCombo;
     @javafx.fxml.FXML
     private TextField inmateIdText;
+    public int price;
 
     @javafx.fxml.FXML
     public void initialize() {
-        inventoryCombo.getItems().addAll("Bed","chair","table");
+        inventoryCombo.getItems().addAll("Bed","Chair","Table");
     }
 
     @javafx.fxml.FXML
     public void calculatebutton(ActionEvent actionEvent) {
+         if (inventoryCombo.getValue().equals("Bed")){
+             price = 200;
+         } else if (inventoryCombo.getValue().equals("Chair")) {
+             price = 300;
+         }else if (inventoryCombo.getValue().equals("Table")){
+             price = 400;
+        }
+         int value= (price+(price*10/100));
+         int remain= 1000-value;
+
+        displayLabel.setText("Inmate ID : "+inmateIdText.getText()+'\n'+"Replacement Value = "+Integer.toString(value)+'\n'+
+                "Inmate new balance = "+Integer.toString(remain));
 
 
+    }
+
+    @javafx.fxml.FXML
+    public void backButtom(ActionEvent actionEvent) {
+
+        Parent root = null;
+
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource( "/com/oop/sneha/simulatingoperationofaprisonmanagement/Nowfal_2120276_accountant_officer/accountant_officer_view.fxml"));
+            root = fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Scene scene = new Scene(root);
+        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("login");
+        stage.show();
     }
 }
